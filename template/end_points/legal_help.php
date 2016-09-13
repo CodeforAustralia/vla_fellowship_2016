c730f6a9277d0392b75b02fd975170eec59caed3
Christian Arevalo <carevaloq87@hotmail.com>
2016-09-13 05:30:23 +0000

    Prototype V1 of eligibility

---
+++ template/end_points/legal_help.php
@@ -0,0 +1,44 @@
+
+         <div class="article-container">
+            <article class="outcome group">
+               <div class="inner group">
+                  <div class="result-info"><input type="hidden" id="outcome" value="Family, not eligible">
+                     <h2 class="result-title"></h2>Legal advice and information is available for this type of problem.</h2>
+                     
+                     <p>Please call us LegalHelp for free advice and information:
+                     <br>Phone <strong>1300 792 387</strong>, Monday to Friday from 8.45 am to 5.15 pm.</br></p>
+                     
+                     <p><strong>If you are experiencing an urgent or emergency situation do not wait, contact the police on '000'.</strong></p>
+                     
+                     <p><h3>Helpful Information and Resources</h3></p>
+                     <p>[Description here]</p>
+
+                     <p><h3>Other Organizations That Can Help</h3></p>
+                     <ul>
+                     <li><a href="#" onClick="trackOutboundLink(this, 'Outbound Links', '#');
+                     return false;">Option 1</a>: [Description here]</li>
+                     <li><a href="#" onClick="trackOutboundLink(this, 'Outbound Links', '#');
+                     return false;">Option 2</a>: [Description here]</li>
+                     <li><a href="#" onClick="trackOutboundLink(this, 'Outbound Links', '#');
+                     return false;">Option 3</a>: [Description here]</li>
+                     
+                     <p>You can also search for a legal center in your area - you may have to pay for advice.</p>
+                     <form method="get" class="find-location-for-service" action="#">
+                        <fieldset>
+                           <legend><strong>Find a legal center</strong><br></legend><label for="postcode">
+                              Enter a VIC postcode
+                              <input class="postcode" id="postcode" name="Location" type="text" placeholder="eg 3000"><input name="Pro" type="hidden" value="False"><select id="AREA" name="UmbrellaLegalIssue" class="dropmenu" style="display:none;">
+                                 <option value="LIUFAM">Family</option></select></label><input type="submit" value="Find" class="button"><br></fieldset>
+                     </form>
+                     <div id="results-intro">
+                        <p>These are the nearest organisations to <span></span>:
+                        </p>
+                     </div>
+                     <div id="search-results"></div>
+                     <div id="search-error"></div>
+                     <p id="indicator" style="display:none">Fetching results...</p>
+                     
+                  </div>
+               </div>
+            </article>
+         </div>
